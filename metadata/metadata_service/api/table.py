0e87710ce96a8bd79a877ccf83b2d22913ac5165
Drew Bittenbender <dbittenbender@yahoo.com>
2023-02-27 09:43:28 -0500

    test

--- metadata/metadata_service/api/table.py
+++ metadata/metadata_service/api/table.py
@@ -4,6 +4,7 @@
 import json
 from http import HTTPStatus
 from typing import Any, Iterable, Mapping, Optional, Union
+import logging
 
 from amundsen_common.entity.resource_type import ResourceType
 from amundsen_common.models.lineage import LineageSchema
@@ -19,6 +20,7 @@ from metadata_service.entity.dashboard_summary import DashboardSummarySchema
 from metadata_service.exception import NotFoundException
 from metadata_service.proxy import get_proxy_client
 
+LOGGER = logging.getLogger(__name__)
 
 class TableDetailAPI(Resource):
     """
@@ -31,12 +33,15 @@ class TableDetailAPI(Resource):
     @swag_from('swagger_doc/table/detail_get.yml')
     def get(self, table_uri: str) -> Iterable[Union[Mapping, int, None]]:
         try:
+            LOGGER.info(f"DREW: table_uri={table_uri}")
             table = self.client.get_table(table_uri=table_uri)
             schema = TableSchema()
             return schema.dump(table), HTTPStatus.OK
 
         except NotFoundException:
             return {'message': 'table_uri {} does not exist'.format(table_uri)}, HTTPStatus.NOT_FOUND
+        except Exception:
+            LOGGER.exception(f"DREW: ERROR={table_uri}")
 
 
 class TableLineageAPI(Resource):
