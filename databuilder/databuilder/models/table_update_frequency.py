3b4c96f39816aeda4c42c82e2659e24f1f2297bd
Drew Bittenbender <drew@foodtruck.ai>
2023-12-07 14:10:48 +0000

    update frequency added to ui

--- databuilder/databuilder/models/table_update_frequency.py
+++ databuilder/databuilder/models/table_update_frequency.py
@@ -14,6 +14,8 @@ LOGGER = logging.getLogger(__name__)
 
 
 class UpdateFrequency(Enum):
+    ANNUALLY = "annually"
+    QUARTERLY = "quarterly"
     MONTHLY = "monthly"
     WEEKLY = "weekly"
     DAILY = "daily"
