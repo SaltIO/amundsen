5662c9b93d305782f03b390baf73cd3612a95704
dbittenbender <dbittenbender@yahoo.com>
2024-02-26 12:29:26 -0500

    Image overrides (#40)
    * u
    
    * u
    
    ---------
    
    Co-authored-by: Drew <drew@cmdrvl.com>

--- frontend/amundsen_application/static/js/config/config.ts
+++ frontend/amundsen_application/static/js/config/config.ts
@@ -2,6 +2,7 @@ import { AppConfig } from './config-types';
 import configDefault from './config-default';
 import configCustom from './config-custom';
 import configHost from './config-host';
+import { ResourceType } from '../interfaces';
 import _ from 'lodash';
 
 // This is not a shallow merge.
@@ -9,20 +10,35 @@ import _ from 'lodash';
 // Any defined members of customHost will override configCustom.
 // const appConfig: AppConfig = { ...configDefault, ...configCustom, ...configHost };
 
-// Use _.merge specifically for the nestedConfig element
+console.log("starting appconfig")
+
+// Merge badges
 const mergedBadges = _.merge(
     {},
     configDefault.badges,
-    configCustom.badges,
-    configHost.badges
+    (configCustom.badges ? configCustom.badges : {}),
+    (configHost.badges ? configHost.badges : {})
 );
+
+// Merge resourceConfig
+const mergedResourceConfig = _.merge(
+    {},
+    configDefault.resourceConfig,
+    configCustom.resourceConfig,
+    configHost.resourceConfig
+);
+
 
 // Perform complete overrides for other properties
 const appConfig: AppConfig = {
     ...configDefault,
     ...configCustom,
     ...configHost,
-    badges: mergedBadges, // Use the merged result for nestedConfig
+    badges: mergedBadges, // Use the merged result
+    resourceConfig: mergedResourceConfig
 };
+
+console.log("AppConfig");
+console.log(JSON.stringify(appConfig, null, 2));
 
 export default appConfig;
