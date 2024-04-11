7436bfbfbd8d173edd23ac3299f17f51e14caa0e
Drew <drew@cmdrvl.com>
2024-04-11 14:57:04 +0000

    u

--- metadata/metadata_service/__init__.py
+++ metadata/metadata_service/__init__.py
@@ -197,6 +197,12 @@ def create_app(*, config_module_class: str) -> Flask:
                      '/data_source/data_provider/<path:data_provider_uri>')
     api.add_resource(FileDetailAPI,
                      '/data_source/file/<path:file_uri>')
+    api.add_resource(FileTagAPI,
+                     '/data_source/file/<path:id>/tag/<tag>')
+    api.add_resource(FileDescriptionAPI,
+                     '/data_source/file/<path:id>/description')
+    api.add_resource(FileOwnerAPI,
+                     '/data_source/file/<path:file_uri>/owner/<owner>')
     api.add_resource(FileLineageAPI,
                      '/data_source/file/<path:file_uri>/lineage')
     app.register_blueprint(api_bp)
