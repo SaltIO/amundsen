5ce341c28aa14247c9c190278a6004c84f37d253
Drew <drew@cmdrvl.com>
2025-06-10 19:37:33 +0000

    u

--- common/amundsen_common/models/util.py
+++ common/amundsen_common/models/util.py
@@ -1,20 +1,39 @@
+from typing import Dict
 from marshmallow import fields, ValidationError
+from marshmallow.base import FieldABC
+from collections.abc import Mapping
+
 
 class UnionSchemaField(fields.Field):
     def __init__(self, union_types, **kwargs):
         super().__init__(**kwargs)
         self._schemas = []
+
         for typ in union_types:
             schema_cls = getattr(typ, "__marshmallow_schema__", None)
-            if schema_cls is None:
+            if schema_cls:
+                self._schemas.append((typ, schema_cls()))
+            elif typ is str:
+                self._schemas.append((typ, fields.String()))
+            elif typ in (dict, Dict, Mapping):
+                self._schemas.append((typ, fields.Dict()))
+            elif typ is int:
+                self._schemas.append((typ, fields.Integer()))
+            elif typ is float:
+                self._schemas.append((typ, fields.Float()))
+            elif typ is bool:
+                self._schemas.append((typ, fields.Boolean()))
+            else:
                 raise ValueError(f"No schema registered for type {typ}")
-            self._schemas.append((typ, schema_cls()))
 
     def _deserialize(self, value, attr, data, **kwargs):
         errors = []
         for typ, schema in self._schemas:
             try:
-                return schema.load(value)
+                if isinstance(schema, FieldABC):
+                    return schema.deserialize(value, attr, data, **kwargs)
+                else:
+                    return schema.load(value)
             except ValidationError as err:
                 errors.append(err.messages)
         raise ValidationError({"union": errors})
@@ -22,5 +41,8 @@ class UnionSchemaField(fields.Field):
     def _serialize(self, value, attr, obj, **kwargs):
         for typ, schema in self._schemas:
             if isinstance(value, typ):
-                return schema.dump(value)
+                if isinstance(schema, FieldABC):
+                    return schema.serialize(attr, {"dummy": value})
+                else:
+                    return schema.dump(value)
         raise ValidationError(f"Value {value} does not match any known type")
